740ae0bd81ff6231b8345f7b4457f67092c11e86
YousefAllam221B <yousef.allam221b@gmail.com>
2022-02-12 21:57:57 +0200

    small design fixes

--- downloader.py
+++ downloader.py
@@ -1,19 +1,27 @@
 import requests
 from requests_ntlm import HttpNtlmAuth
 import bs4 as bs
+path = r'C:\Users\Youse\Documents\Semseter 6'
 
 # Making a get request
-Home_Page_Response = requests.get('https://cms.guc.edu.eg/apps/student/HomePageStn.aspx',
-            auth = HttpNtlmAuth('yousef.alam', 'JohnWatson123!')).text
+def getRequests(path):
+    recieve = requests.get(path, auth = HttpNtlmAuth('yousef.alam', 'JohnWatson123!'))
+    return recieve
 
-Home_Page_Html = bs.BeautifulSoup(Home_Page_Response, 'html.parser')
-Html_Files = []
-# Getting Courses info Courses table
-tableID = 'ContentPlaceHolderright_ContentPlaceHoldercontent_GridViewcourses'
-CoursesNames = Home_Page_Html.select(tableID + ' td:nth-child(2)')
-CoursesIDs = Home_Page_Html.select(tableID + ' td:nth-child(5)')
-StudentIDs = Home_Page_Html.select(tableID + ' td:nth-child(6)')
+def downloadFiles(name, exe):
+        with open(name + '.' + exe,'wb') as f:
+            f.write(recieve.content)
 
-for i in range(len(CoursesNames)):
-    CourseDict = {'Name': CoursesNames[i].getText(), 'ID': CoursesIDs[i].getText(), 'SID': StudentIDs[i].getText()}
-    Html_Files.append(CourseDict)
+def GetCoursesInfoDict():
+    Home_Page_Response = getRequests('https://cms.guc.edu.eg/apps/student/HomePageStn.aspx')
+    Home_Page_Html = bs.BeautifulSoup(Home_Page_Response, 'html.parser')
+    Html_Files = []
+    # Getting Courses info Courses table
+    tableID = 'ContentPlaceHolderright_ContentPlaceHoldercontent_GridViewcourses'
+    CoursesNames = Home_Page_Html.select(tableID + ' td:nth-child(2)')
+    CoursesIDs = Home_Page_Html.select(tableID + ' td:nth-child(5)')
+    StudentIDs = Home_Page_Html.select(tableID + ' td:nth-child(6)')
+    for i in range(len(CoursesNames)):
+        CourseDict = {'Name': CoursesNames[i].getText(), 'ID': CoursesIDs[i].getText(), 'SID': StudentIDs[i].getText()}
+        Html_Files.append(CourseDict)
+    return Html_Files
