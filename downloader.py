5d32bed9cefc75ce1617f1b30a60077b3ba18a0e
YousefAllam221B <yousef.allam221b@gmail.com>
2022-02-12 22:07:21 +0200

    small fix

--- downloader.py
+++ downloader.py
@@ -4,20 +4,19 @@ import bs4 as bs
 path = r'C:\Users\Youse\Documents\Semseter 6'
 
 # Making a get request
-def getRequests(path):
+def getRequest(path):
     recieve = requests.get(path, auth = HttpNtlmAuth('yousef.alam', 'JohnWatson123!'))
-    return recieve
+    return recieve.text
 
 def downloadFiles(name, exe):
         with open(name + '.' + exe,'wb') as f:
             f.write(recieve.content)
-
-def GetCoursesInfoDict():
-    Home_Page_Response = getRequests('https://cms.guc.edu.eg/apps/student/HomePageStn.aspx')
+def getCoursesInfoDict():
+    Home_Page_Response = getRequest('https://cms.guc.edu.eg/apps/student/HomePageStn.aspx')
     Home_Page_Html = bs.BeautifulSoup(Home_Page_Response, 'html.parser')
     Html_Files = []
     # Getting Courses info Courses table
-    tableID = 'ContentPlaceHolderright_ContentPlaceHoldercontent_GridViewcourses'
+    tableID = '#ContentPlaceHolderright_ContentPlaceHoldercontent_GridViewcourses'
     CoursesNames = Home_Page_Html.select(tableID + ' td:nth-child(2)')
     CoursesIDs = Home_Page_Html.select(tableID + ' td:nth-child(5)')
     StudentIDs = Home_Page_Html.select(tableID + ' td:nth-child(6)')
