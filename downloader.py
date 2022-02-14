f73de9e6097a4df0c46b42664cd57101cfbafc4c
YousefAllam221B <yousef.allam221b@gmail.com>
2022-02-14 22:55:48 +0200

    Downloads all content on CMS

--- downloader.py
+++ downloader.py
@@ -1,18 +1,17 @@
 import requests
 from requests_ntlm import HttpNtlmAuth
 import bs4 as bs
+import re
+import os
 path = r'C:\Users\Youse\Documents\Semseter 6'
 
 # Making a get request
 def getRequest(path):
     recieve = requests.get(path, auth = HttpNtlmAuth('yousef.alam', 'JohnWatson123!'))
-    return recieve.text
+    return recieve
 
-def downloadFiles(name, exe):
-        with open(name + '.' + exe,'wb') as f:
-            f.write(recieve.content)
 def getCoursesInfoDict():
-    Home_Page_Response = getRequest('https://cms.guc.edu.eg/apps/student/HomePageStn.aspx')
+    Home_Page_Response = getRequest('https://cms.guc.edu.eg/apps/student/HomePageStn.aspx').text
     Home_Page_Html = bs.BeautifulSoup(Home_Page_Response, 'html.parser')
     Html_Files = []
     # Getting Courses info Courses table
@@ -21,6 +20,44 @@ def getCoursesInfoDict():
     CoursesIDs = Home_Page_Html.select(tableID + ' td:nth-child(5)')
     StudentIDs = Home_Page_Html.select(tableID + ' td:nth-child(6)')
     for i in range(len(CoursesNames)):
-        CourseDict = {'Name': CoursesNames[i].getText(), 'ID': CoursesIDs[i].getText(), 'SID': StudentIDs[i].getText()}
+
+        CourseDict = {'Name': re.sub('[|()]', '', CoursesNames[i].getText()) , 'ID': CoursesIDs[i].getText(), 'SID': StudentIDs[i].getText()}
         Html_Files.append(CourseDict)
     return Html_Files
+
+def downloadFile(dir, name, path, ext):
+    recieve = getRequest(path)
+    with open(dir + name + ext,'wb') as f:
+        f.write(recieve.content)
+
+def downloadCourseContent(dirName, id, sid):
+    Course_Page_Response = getRequest('https://cms.guc.edu.eg/apps/student/CourseViewStn.aspx?id={id}&sid={sid}'.format(id = id, sid = sid)).text
+    Course_Page_Html = bs.BeautifulSoup(Course_Page_Response, 'html.parser')
+    downloadableContent = Course_Page_Html.select('.page-title-wrapper #download')
+    downloadableContentLabel = Course_Page_Html.select('.page-title-wrapper [class=card-body] div strong')
+
+    for i in range(len(downloadableContent)):
+        name = downloadableContentLabel[i].getText()[4:].lstrip()
+        path = downloadableContent[i].get('href')
+        filename, ext = os.path.splitext(downloadableContent[i].get('href'))
+        if (ext != '.mp4' and ext != '.png'):
+
+            if ('lecture' in name.lower()):
+                downloadFile(dirName + '/Lectures/', name, 'https://cms.guc.edu.eg/' + path,  ext)
+            elif ('practice assignment' in name.lower()):
+                downloadFile(dirName + '/Practice Assignments/', name, 'https://cms.guc.edu.eg/' + path,  ext)
+            else:
+                downloadFile(dirName + '/Others/', name, 'https://cms.guc.edu.eg/' + path,  ext)
+
+def downloadAllCourses():
+    CoursesInfo = getCoursesInfoDict()
+    for course in CoursesInfo:
+        print(course['Name'], course['ID'], course['SID'])
+        downloadCourseContent(course['Name'], course['ID'], course['SID'])
+        print('Done')
+downloadAllCourses()
+print("All Done")
+
+
+
+#
